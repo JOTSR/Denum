e980f6c25e944b4e1294beea219422f87e76d3a8
Julien <xdrz.jot.sr@gmail.com>
2021-09-19 21:57:29 +0200

    build: Moving dependencies to ./deps.ts and updating GitHub actions

--- abs.test.ts
+++ abs.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.106.0/testing/asserts.ts'
+import { assertEquals } from './deps.ts'
 import { abs } from './abs.ts'
 
 Deno.test({
--- round.test.ts
+++ round.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.106.0/testing/asserts.ts'
+import { assertEquals } from './deps.ts'
 import { round } from './round.ts'
 
 Deno.test({
