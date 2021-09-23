7e27cbf62ead6f0ec04d2e61162b61f44fa1625a
Julien Oucli <xdrz.jot.sr@gmail.com>
2021-09-23 13:42:05 +0200

    refactor: overloading instead of conditional parameters

--- random.ts
+++ random.ts
@@ -8,16 +8,12 @@ export const random = (min: number, max: number) => {
   return Math.random() * (max - min) + min;
 };
 
-/**
- * Genearate a bounded random integer
- * @param min Minimum value
- * @param max Maximum value
- * @returns Random interger
- */
-export const randomInt = (
+function _randomInt(min: number, max: number): number;
+function _randomInt(min: BigInt, max: BigInt): BigInt;
+function _randomInt(
   min: number | BigInt,
   max: number | BigInt,
-): number | BigInt => {
+): number | BigInt {
   //BigInt
   if (typeof (min) == 'bigint' && typeof (max) == 'bigint') {
     return BigInt(
@@ -33,7 +29,15 @@ export const randomInt = (
     return Math.random() * (max - min) + min;
   }
   throw new TypeError('Parameters must be of the same type');
-};
+}
+
+/**
+ * Genearate a bounded random integer
+ * @param min Minimum value
+ * @param max Maximum value
+ * @returns Random interger
+ */
+ export const randomInt = _randomInt;
 
 /**
  * Produce an array filled with random values
@@ -49,6 +53,22 @@ export const randomArray = (min: number, max: number, length: number) => {
 
 function _randomIntArray(min: number, max: number, length: number): number[];
 function _randomIntArray(min: BigInt, max: BigInt, length: number): BigInt[];
+function _randomIntArray(
+  min: number | BigInt,
+  max: number | BigInt,
+  length: number,
+) {
+  if (typeof min === 'number' && typeof max === "number") {
+    const array = new Array<typeof min>(length).map((_) => randomInt(min, max));
+    return array as typeof min[];
+  }
+  if (typeof min === 'bigint' && typeof max === "bigint") {
+    const array = new Array<typeof min>(length).map((_) => randomInt(min, max));
+    return array as typeof min[];
+  }
+  return [] as typeof min[];
+}
+
 /**
  * Produce an array filled with random integers
  * @param min Minimum value
@@ -56,13 +76,4 @@ function _randomIntArray(min: BigInt, max: BigInt, length: number): BigInt[];
  * @param length Array length
  * @returns Rando integers array
  */
-function _randomIntArray(
-  min: number | BigInt,
-  max: number | BigInt,
-  length: number,
-) {
-  const array = new Array<typeof min>(length).map((_) => randomInt(min, max));
-  return array as typeof min[];
-}
-
 export const randomIntArray = _randomIntArray;
