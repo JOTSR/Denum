9506455157d8e3e659693fd2793118e98b7d4d73
Julien <xdrz.jot.sr@gmail.com>
2021-10-24 11:29:13 +0200

    feat: update exported modules

--- mod.ts
+++ mod.ts
@@ -1,4 +1,6 @@
+export { abs } from './abs.ts';
+export { clamp } from './clamp.ts'
+export { map } from './map.ts'
+export { random, randomArray, randomInt, randomIntArray } from './random.ts';
 export { irange, range } from './range.ts';
 export { round } from './round.ts';
-export { random, randomArray, randomInt, randomIntArray } from './random.ts';
-export { abs } from './abs.ts';
