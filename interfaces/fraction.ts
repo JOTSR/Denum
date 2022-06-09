6dc6899b392588de87e7cd9a7097b82f1481cd95
Julien Oucli <xdrz.jot.sr@gmail.com>
2022-06-09 19:26:46 +0200

    fix!: rename method and update doc

--- interfaces/fraction.ts
+++ interfaces/fraction.ts
@@ -1,301 +1,337 @@
-import { abs, gcd, sign } from '../mod.ts';
-import { AbstractValue } from './value.ts';
+import { abs, gcd, sign } from '../mod.ts'
+import { AbstractValue } from './value.ts'
 
 export class Frac extends AbstractValue<Frac> {
-    #num: bigint;
-    #den: bigint;
+	#num: bigint
+	#den: bigint
 
-    /**
-     * If the input is a number or string, convert it to a fraction, otherwise, just use the input as is
-     * The `repeating` parameter is used to indicate whether the input is a repeating decimal or not
-     * @param {number | string | { num: bigint, den: bigint }} n - number | string | { num: bigint; den: bigint }
-     * @param [repeating=false] - boolean
-     */
-    constructor(
-        n: number | string | { num: bigint; den: bigint },
-        repeating = false,
-    ) {
-        super();
-        if (typeof n === 'number' || typeof n === 'string') {
-            n = Frac.fromNumber(n, repeating);
-        }
-        this.#num = n.num * sign(n.den);
-        this.#den = abs(n.den);
-    }
+	/**
+	 * The constructor function takes two arguments, `num` and `den`, and sets the numerator and
+	 * denominator of the rational number to the absolute value of `num` and `den`, respectively, and
+	 * the sign of the numerator to the sign of Frac
+	 * @param {bigint} num - The numerator of the rational number.
+	 * @param {bigint} den - The denominator of the rational number.
+	 */
+	constructor(num: bigint, den: bigint) {
+		super()
+		this.#num = num * sign(den)
+		this.#den = abs(den)
+	}
 
-    /**
-     * get fraction numerator
-     */
-    get num() {
-        return this.#num;
-    }
+	/**
+	 * get fraction numerator
+	 */
+	get num() {
+		return this.#num
+	}
 
-    /**
-     * get fraction denominator
-     */
-    get den() {
-        return this.#den;
-    }
+	/**
+	 * get fraction denominator
+	 */
+	get den() {
+		return this.#den
+	}
 
-    /**
-     * get fraction value {num, den}
-     */
-    get value() {
-        return { num: this.#num, den: this.#den };
-    }
+	/**
+	 * get fraction value {num, den}
+	 */
+	get value() {
+		return { num: this.#num, den: this.#den }
+	}
 
-    /**
-     * It takes a number or string, and returns a Frac object
-     * @param {number | string} n - number (float or int) | string ("xxx.xxx" or "num/den")
-     * @param [repeating=false] - boolean
-     * @returns the corresponding fraction
-     */
-    static fromNumber(n: number | string, repeating = false): Frac {
-        if (typeof n === 'number' && n.toString().length > 15) {
-            n = n.toString().slice(0, -1);
-            console.warn(
-                'decimals > 15, consider using string instead of number due to float32 precision limits',
-            );
-        }
-        n = n.toString();
+	/**
+	 * It takes a number or string, and returns a Frac object
+	 * @param {number | string} n - number (float or int) | string ("xxx.xxx" or "num/den")
+	 * @param [repeating=false] - boolean
+	 * @returns the corresponding fraction
+	 */
+	static fromFloat(n: number | string, repeating = false): Frac {
+		if (typeof n === 'number' && n.toString().length > 15) {
+			n = n.toString().slice(0, -1)
+			console.warn(
+				'decimals > 15, consider using string instead of number due to float32 precision limits'
+			)
+		}
+		n = n.toString()
 
-        const sign = n.includes('-') ? -1n : 1n;
-        n = n.replace('-', '');
+		const sign = n.includes('-') ? -1n : 1n
+		n = n.replace('-', '')
 
-        const decimals = BigInt(n.split('.')[1]?.length ?? 0);
+		const decimals = BigInt(n.split('.')[1]?.length ?? 0)
 
-        if (decimals === 0n) return new Frac({ num: BigInt(n), den: 1n });
+		if (decimals === 0n) return new Frac(BigInt(n), 1n)
 
-        const denominator = 10n ** decimals - (repeating ? 1n : 0n);
-        const numerator = BigInt(n.replace('.', '')) -
-            (repeating ? BigInt(n.split('.')[0]) : 0n);
-        const _gcd = gcd(numerator, denominator);
-        return new Frac({
-            num: sign * numerator / _gcd,
-            den: denominator / _gcd,
-        });
-    }
+		const denominator = 10n ** decimals - (repeating ? 1n : 0n)
+		const numerator =
+			BigInt(n.replace('.', '')) -
+			(repeating ? BigInt(n.split('.')[0]) : 0n)
+		const _gcd = gcd(numerator, denominator)
+		return new Frac((sign * numerator) / _gcd, denominator / _gcd)
+	}
 
-    /**
-     * It takes a string of shape "int/int" and returns a fraction
-     * @param {string} str - string
-     * @returns A new instance of the Frac class
-     */
-    static fromString(str: string): Frac {
-        if (str.includes('/')) {
-            const num = BigInt(str.split('/')[0]);
-            const den = BigInt(str.split('/')[1]);
-            return new Frac({ num, den });
-        }
-        throw new TypeError(`Invalid string ${str}`);
-    }
+	/**
+	 * It takes a string of shape "int/int" and returns a fraction
+	 * @param {string} str - string
+	 * @returns A new instance of the Frac class
+	 */
+	static fromString(str: string): Frac {
+		if (str.includes('/')) {
+			const num = BigInt(str.split('/')[0])
+			const den = BigInt(str.split('/')[1])
+			return new Frac(num, den)
+		}
+		throw new TypeError(`Invalid string ${str}`)
+	}
 
-    /**
-     * It takes a string in the form of a LaTeX fraction, and returns a Frac object
-     * @param {string} latex - string
-     * @returns A new instance of the Frac class.
-     */
-    static fromLatex(latex: string): Frac {
-        const sign = latex.match(/(\\left\(\s*-\s*).+(\\right\))/i) ? -1n : 1n;
-        const match = latex.match(/frac{(\d+)}{(\d+)}/i);
-        if (match?.length === 3) {
-            const [_, num, den] = match as [string, string, string];
-            return new Frac({ num: sign * BigInt(num), den: BigInt(den) });
-        }
-        throw new TypeError(`Unable to parse latex ${latex}`);
-    }
+	/**
+	 * It takes a JSON string or object, and returns a new Frac object
+	 * @param {string | Record<string, bigint>} json - string | Record<string, unknown>
+	 * @returns A new instance of the Frac class.
+	 */
+	static fromJSON(json: string | Record<string, bigint>): Frac {
+		const { num, den } =
+			typeof json === 'string'
+				? JSON.parse(json)
+				: (json as { num: bigint; den: bigint })
+		return new Frac(num, den)
+	}
 
-    /**
-     * Return the simplified fraction sum from an array of fractions
-     * @param {Frac[]} frac - Frac[]
-     * @returns simplified sum Frac
-     */
-    static add(...frac: Frac[]): Frac {
-        const result = frac.reduce((prev, curr) => {
-            const num = prev.num * curr.den + curr.num * prev.den;
-            const den = prev.den * curr.den;
-            return new Frac({ num, den });
-        }, new Frac(0));
-        return Frac.simplify(result);
-    }
+	/**
+	 * It takes a string in the form of a LaTeX fraction, and returns a Frac object
+	 * @param {string} latex - string
+	 * @returns A new instance of the Frac class.
+	 */
+	static fromLatex(latex: string): Frac {
+		const sign = latex.match(/(\\left\(\s*-\s*).+(\\right\))/i) ? -1n : 1n
+		const match = latex.match(/frac{(\d+)}{(\d+)}/i)
+		if (match?.length === 3) {
+			const [_, num, den] = match as [string, string, string]
+			return new Frac(sign * BigInt(num), BigInt(den))
+		}
+		throw new TypeError(`Unable to parse latex ${latex}`)
+	}
 
-    /**
-     * Return the simplified fraction subtraction of the first item by all followings fractions
-     * @param {Frac[]} frac - Frac[]
-     * @returns simplified sub Frac
-     */
-    static sub(...frac: Frac[]): Frac {
-        const result = Frac.add(
-            frac[0],
-            ...frac
-                .slice(1)
-                .map((frac) => new Frac({ num: -frac.num, den: frac.den })),
-        );
-        return Frac.simplify(result);
-    }
+	//Maths
 
-    /**
-     * Return the simplified fraction product from an array of fractions
-     * @param {Frac[]} frac - Frac[]
-     * @returns simplified product Frac
-     */
-    static multiply(...frac: Frac[]): Frac {
-        const result = frac.reduce(
-            (prev, curr) =>
-                new Frac({
-                    num: prev.num * curr.num,
-                    den: prev.den * curr.den,
-                }),
-            new Frac(1),
-        );
-        return Frac.simplify(result);
-    }
+	/**
+	 * returns a new fraction that is the sum of the current Frac and all args
+	 * @param {Frac[]} frac - Frac[]
+	 * @returns The return value is a new Frac
+	 */
+	add(...frac: Frac[]): Frac {
+		return [this, ...frac]
+			.reduce((prev, curr) => {
+				const num = prev.num * curr.den + curr.num * prev.den
+				const den = prev.den * curr.den
+				return new Frac(num, den)
+			}, new Frac(0n, 1n))
+			.simplify()
+	}
 
-    /**
-     * Return the simplified fraction quotient by dividing the first item by all the followings
-     * @param {Frac[]} frac - Frac[]
-     * @returns simplified quotient Frac
-     */
-    static divide(...frac: Frac[]): Frac {
-        const result = Frac.multiply(
-            frac[0],
-            ...frac
-                .slice(1)
-                .map((frac) => new Frac({ num: frac.den, den: frac.num })),
-        );
-        return Frac.simplify(result);
-    }
+	/**
+	 * returns a new fraction that is the subtraction of the current Frac by all args
+	 * @param {Frac[]} frac - Frac[]
+	 * @returns The return value is a new Frac
+	 */
+	sub(...frac: Frac[]): Frac {
+		return this.sub(...frac)
+	}
 
-    /**
-     * It takes a fraction, and returns a fraction with the same value, but with the numerator and
-     * denominator reduced to their lowest common terms
-     * @param {Frac} frac - The fraction to simplify
-     * @returns simplified Frac
-     */
-    static simplify(frac: Frac): Frac {
-        const { num, den } = frac;
-        const _gcd = gcd(num, den);
-        return new Frac({ num: num / _gcd, den: den / _gcd });
-    }
+	/**
+	 * returns a new fraction that is the product of the current Frac and all args
+	 * @param {Frac[]} frac - Frac[]
+	 * @returns The return value is a new Frac
+	 */
+	multiply(...frac: Frac[]): Frac {
+		return [this, ...frac]
+			.reduce(
+				(prev, curr) =>
+					new Frac(prev.num * curr.num, prev.den * curr.den),
+				new Frac(1n, 1n)
+			)
+			.simplify()
+	}
 
-    /**
-     * It takes a fraction and returns a new fraction that is the inverse of the original fraction
-     * @param {Frac} frac - The fraction to invert.
-     * @returns A new Frac object with the numerator and denominator swapped.
-     */
-    static invert(frac: Frac): Frac {
-        return new Frac({ num: frac.den, den: frac.num });
-    }
+	/**
+	 * returns a new fraction that is the quotient of the current Frac by all args
+	 * @param {Frac[]} frac - Frac[]
+	 * @returns The return value is a new Frac
+	 */
+	divide(...frac: Frac[]): Frac {
+		return this.multiply(...frac.map((frac) => frac.invert())).simplify()
+	}
 
-    /**
-     * It returns a new fraction that is the opposite of the fraction passed in
-     * @param {Frac} frac - Frac
-     * @returns A new Frac object with the opposite sign of the original Frac object.
-     */
-    static opposite(frac: Frac): Frac {
-        return new Frac({ num: -frac.num, den: frac.den });
-    }
+	/**
+	 * It returns a new fraction that is the same as the original fraction, but simplified
+	 * @returns The simplified fraction.
+	 */
+	simplify(): Frac {
+		const { num, den } = this
+		const _gcd = gcd(num, den)
+		return new Frac(num / _gcd, den / _gcd)
+	}
 
-    /**
-     * returns a new fraction that is the sum of the current Frac and all args
-     * @param {Frac[]} frac - Frac[]
-     * @returns The return value is a new Frac
-     */
-    add(...frac: Frac[]): Frac {
-        return Frac.add(this, ...frac);
-    }
+	/**
+	 * It returns the inverse of the fraction.
+	 * @returns The inverse of the fraction.
+	 */
+	invert(): Frac {
+		return new Frac(this.den, this.num)
+	}
 
-    /**
-     * returns a new fraction that is the subtraction of the current Frac by all args
-     * @param {Frac[]} frac - Frac[]
-     * @returns The return value is a new Frac
-     */
-    sub(...frac: Frac[]): Frac {
-        return Frac.sub(this, ...frac);
-    }
+	/**
+	 * It returns the opposite of the fraction
+	 * @returns The opposite of the fraction.
+	 */
+	opposite(): Frac {
+		return new Frac(-this.num, this.den)
+	}
 
-    /**
-     * returns a new fraction that is the product of the current Frac and all args
-     * @param {Frac[]} frac - Frac[]
-     * @returns The return value is a new Frac
-     */
-    multiply(...frac: Frac[]): Frac {
-        return Frac.multiply(this, ...frac);
-    }
+	/**
+	 * If the number is less than zero, return negative one, otherwise return positive one.
+	 * @returns The sign of the number.
+	 */
+	sign(): -1n | 1n {
+		return this.#num < 0n ? -1n : 1n
+	}
 
-    /**
-     * returns a new fraction that is the quotient of the current Frac by all args
-     * @param {Frac[]} frac - Frac[]
-     * @returns The return value is a new Frac
-     */
-    divide(...frac: Frac[]): Frac {
-        return Frac.divide(this, ...frac);
-    }
+	/**
+	 * Returns a new fraction object with the absolute value of the numerator
+	 * and denominator
+	 * @returns A new Frac object with the absolute value of the numerator and denominator.
+	 */
+	abs(): Frac {
+		return new Frac(abs(this.#num), this.#den)
+	}
 
-    /**
-     * It returns a new fraction that is the same as the original fraction, but simplified
-     * @returns The simplified fraction.
-     */
-    simplify(): Frac {
-        return Frac.simplify(this);
-    }
+	//Logic
 
-    /**
-     * It returns the inverse of the fraction.
-     * @returns The inverse of the fraction.
-     */
-    invert(): Frac {
-        return Frac.invert(this);
-    }
+	/**
+	 * If the simplified denominator is 1, then the fraction is an integer.
+	 * @returns a boolean value.
+	 */
+	isInt(): boolean {
+		return this.simplify().#den === 1n
+	}
 
-    /**
-     * It returns the opposite of the fraction
-     * @returns The opposite of the fraction.
-     */
-    opposite() {
-        return Frac.opposite(this);
-    }
+	/**
+	 * It compares two fractions and returns 0 if they are equal, 1 if the first is greater than the
+	 * second, and -1 if the first is less than the second.
+	 * @param {Frac} compared - The fraction to compare to.
+	 * @returns The sign of the difference between the two fractions.
+	 */
+	compare(compared: Frac): -1n | 0n | 1n {
+		const { num: cNum, den: cDen } = compared.simplify().value
+		const { num, den } = this.simplify().value
+		if (num === cNum && den === cDen) return 0n
+		return this.sub(compared).sign()
+	}
 
-    /**
-     * If the numerator and denominator are within the range of a 32 bit float, and the denominator is
-     * not zero, return the float value of the numerator divided by the denominator
-     * @param {bigint} decimals - Number of significant decimals (0-16)
-     * @returns A function that returns a float.
-     */
-    toFloat(decimals = 16n): number {
-        if (decimals < 0n || decimals > 16n) {
-            throw new RangeError(`Output decimals must`);
-        }
-        if (
-            Number.MAX_SAFE_INTEGER > this.#den &&
-            Number.MIN_SAFE_INTEGER < this.#den &&
-            Number.MAX_SAFE_INTEGER > this.#num &&
-            Number.MIN_SAFE_INTEGER < this.#num &&
-            this.#den !== 0n
-        ) {
-            return Number(this.#num) / Number(this.#den);
-        }
-        throw new RangeError(
-            `${this.toString()} cannot be converted to float 32, out of range`,
-        );
-    }
+	/**
+	 * Returns true if the two fractions are equal, false otherwise.
+	 * @param {Frac} compared - The fraction to compare to.
+	 * @returns A boolean value.
+	 */
+	isEqual(compared: Frac): boolean {
+		return this.compare(compared) === 0n
+	}
 
-    /**
-     * It returns a string representation of the fraction.
-     * @returns The numerator and denominator of the fraction.
-     */
-    toString(): string {
-        return `${this.#num}/${this.#den}`;
-    }
+	/**
+	 * Returns true if the current fraction is greater than the compared fraction, or if the compared
+	 * fraction is equal to the current fraction and ifEqual is true.
+	 * @param {Frac} compared - The fraction to compare to.
+	 * @param [ifEqual=false] - If true, the function will return true if the compared fraction is
+	 * equal to the current fraction.
+	 * @returns A boolean value.
+	 */
+	isGreater(compared: Frac, ifEqual = false): boolean {
+		if (ifEqual && this.compare(compared) === 0n) return true
+		return this.compare(compared) === 1n
+	}
 
-    /**
-     * If the numerator is negative, return the fraction wrapped in parentheses. Otherwise, return the
-     * fraction as a LaTeX string
-     * @returns A string.
-     */
-    toLatex(): string {
-        const frac = `\\frac{${abs(this.#num)}}{${this.#den}}`;
-        if (sign(this.#num) === -1n) return `\\left( - ${frac} \\right)`;
-        return frac;
-    }
+	/**
+	 * Returns true if the current fraction is lesser than the compared fraction, or if the compared
+	 * fraction is equal to the current fraction and the ifEqual parameter is true.
+	 * @param {Frac} compared - The fraction to compare to.
+	 * @param [ifEqual=false] - If true, the function will return true if the compared fraction is
+	 * equal to the current fraction.
+	 * @returns The return value is a boolean.
+	 */
+	isLesser(compared: Frac, ifEqual = false): boolean {
+		if (ifEqual && this.compare(compared) === 0n) return true
+		return this.compare(compared) === -1n
+	}
+
+	/**
+	 * It checks if the fraction is between two other fractions.
+	 * @param {Frac} min - The minimum value to compare to.
+	 * @param {Frac} max - The maximum value of the range.
+	 * @param [includeBounds=false] - If true, the bounds are included in the comparison.
+	 * @returns A boolean value
+	 */
+	between(min: Frac, max: Frac, includeBounds = false): boolean {
+		return (
+			this.isGreater(min, includeBounds) &&
+			this.isLesser(max, includeBounds)
+		)
+	}
+
+	//Conversion
+
+	/**
+	 * If the numerator and denominator are within the range of a 32 bit float, and the denominator is
+	 * not zero, return the float value of the numerator divided by the denominator
+	 * @param {bigint} decimals - Number of significant decimals (0-16)
+	 * @returns A function that returns a float.
+	 */
+	toFloat(decimals = 16n): number {
+		if (decimals < 0n || decimals > 16n)
+			throw new RangeError(
+				`Output decimals must be between 0 and 16, not ${decimals}`
+			)
+		if (
+			Number.MAX_SAFE_INTEGER > this.#den &&
+			Number.MIN_SAFE_INTEGER < this.#den &&
+			Number.MAX_SAFE_INTEGER > this.#num &&
+			Number.MIN_SAFE_INTEGER < this.#num &&
+			this.#den !== 0n
+		) {
+			return (
+				Math.round(
+					Number(this.#num * 10n ** decimals) / Number(this.#den)
+				) /
+				10 ** Number(decimals)
+			)
+		}
+		throw new RangeError(
+			`${this.toString()} cannot be converted to float 32, out of range`
+		)
+	}
+
+	/**
+	 * It returns a string representation of the fraction.
+	 * @returns The numerator and denominator of the fraction.
+	 */
+	toString(): string {
+		return `${this.#num}/${this.#den}`
+	}
+
+	/**
+	 * If the numerator is negative, return the fraction wrapped in parentheses. Otherwise, return the
+	 * fraction as a LaTeX string
+	 * @returns A string.
+	 */
+	toLatex(): string {
+		const frac = `\\frac{${abs(this.#num)}}{${this.#den}}`
+		if (sign(this.#num) === -1n) return `\\left( - ${frac} \\right)`
+		return frac
+	}
+
+	/**
+	 * The function returns a string that is a JSON representation of the object
+	 * @returns A string representation of the object.
+	 */
+	toJSON(): string {
+		return `{num: ${this.#num}, den: ${this.#den}}`
+	}
 }
